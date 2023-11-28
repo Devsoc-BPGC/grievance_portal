2a490f6c456b3d676adfa8668017f05a623b11a3
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2023-11-28 18:20:18 +0530

    logo

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
 import formFields from "../formFields";
 import Component from "../components/Component";
 import {
-  Button
+  Button, Typography
 } from "@material-tailwind/react";
 import {Link} from "react-router-dom";
-
+import logo from '../assets/logo.png';
 
 function Dashboard() {
   
@@ -12,6 +12,12 @@ function Dashboard() {
     <div className="flex flex-row items-center justify-center text-center bg-black min-h-screen mr-auto ml-auto">
         <div className="w-1/3 h-full flex flex-col items-center justify-center text-center">
           <header className="text-8xl font-extrabold items-center justify-center text-center p-9 text-white">Mirage</header>
+          <div className="flex flex-row items-center justify-center text-center gap-3">
+            <img width={"10%"} src={logo} alt="BITS Pilani KK Birla Goa Campus" />
+            <Typography className="items-center justify-center text-center text-white">
+              BITS Pilani K.K. Birla Goa Campus
+            </Typography>
+          </div>
         </div>
       <div className="w-2/3 flex flex-col items-center justify-center text-center mt-8 gap-5 mr-auto ml-auto">
         <Link to="presidents-hour"  className="w-1/4"><Button className="w-full">President's Hour</Button></Link>
