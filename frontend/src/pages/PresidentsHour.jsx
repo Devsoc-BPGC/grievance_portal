814c76899a1b2f47569b7e12783d87cdb717ee35
Manamrit Singh <amrit20eclectic@gmail.com>
2023-11-28 19:42:08 +0530

    Added checks for success and failure.

--- frontend/src/pages/PresidentsHour.jsx
+++ frontend/src/pages/PresidentsHour.jsx
@@ -1,51 +1,76 @@
-import React from "react"
+import React from "react";
 import {
-    Card,
-    Input,
-    Button,
-    Typography,
-    Textarea,
-    Alert,
-  } from "@material-tailwind/react";
-  import { useNavigate } from 'react-router-dom';
-  import Snackbar from '@mui/material/Snackbar';
+  Card,
+  Input,
+  Button,
+  Typography,
+  Textarea,
+  Alert,
+} from "@material-tailwind/react";
+import { useNavigate } from 'react-router-dom';
+import Snackbar from '@mui/material/Snackbar';
 
-export default function PresidentsHour(){
-    const [formData, updateFormData]=React.useState({
-        name:"",
-        email:"",
-        phoneNumber:"",
-        desc:"",
-        category:"message"
-      })
-      const [open, toggleSnackbar]=React.useState(false)
-      const navigate = useNavigate();
-      function handleChange(event){
-        updateFormData({
-          ...formData,
-          [event.target.name]:event.target.value
-        })
-       // console.log(formData)
-      }
-      React.useEffect(()=>{
-      //
-      }, [formData])
-      function handleSubmit(event){
-        event.preventDefault()
-        console.log(formData)
-        //make api request here
-        toggleSnackbar(true)
-        updateFormData({
-          name:"",
-        email:"",
-        phoneNumber:"",
-        desc:"",
-        category:"message",
-        })
+export default function PresidentsHour() {
+  const [formData, updateFormData] = React.useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    desc: "",
+    category: "message"
+  })
+  const [open, toggleSnackbar] = React.useState(false)
+  const navigate = useNavigate();
+  const [error, setError] = React.useState("");
+  
+  function handleChange(event) {
+    updateFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  React.useEffect(() => {
+    //
+  }, [formData])
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (
+      formData.name === "" ||
+      formData.email === "" ||
+      formData.phoneNumber === "" ||
+      formData.desc === ""
+    ) {
+      setError("Please fill all fields!");
+      toggleSnackbar(true);
+      return;
     }
-    return(
-        <div className="flex bg-black items-center justify-center text-center h-screen">
-        <Card color="transparent" shadow={false}>
+
+    console.log(formData);
+    // make api request here
+
+    // Display success message
+    window.alert("Success!");
+
+    // Reset form data after submission
+    updateFormData({
+      name: "",
+      email: "",
+      phoneNumber: "",
+      desc: "",
+      category: "message",
+    });
+  }
+
+
+  React.useEffect(() => {
+    setError("");
+  }, [formData]);
+
+  return (
+    <div className="flex bg-black items-center justify-center text-center h-screen">
+      <Card color="transparent" shadow={false}>
         <Typography className="items-center justify-center text-center text-white" variant="h3">
           PRESIDENT'S HOUR
         </Typography>
@@ -87,10 +112,9 @@ export default function PresidentsHour(){
               label="Phone No."
               className=""
               labelProps={{
-                className:
-                  "",
+                className: "",
               }}
-              required={true} 
+              required={true}
               name="phoneNumber"
               value={formData.phoneNumber}
               onChange={handleChange}
@@ -104,22 +128,23 @@ export default function PresidentsHour(){
               name="desc"
               value={formData.desc}
               onChange={handleChange}
-              
             />
           </div>
           <Button type="submit" className="mt-6" fullWidth onClick={handleSubmit}>
             Submit Message
           </Button>
-           <Button className="mt-6" fullWidth onClick={()=>{navigate('/')}}>
+          <Button className="mt-6" fullWidth onClick={() => navigate('/')}>
             Go to dashboard
           </Button>
-          <Snackbar open={open} autoHideDuration={6000} onClose={()=>{toggleSnackbar(false)}}>
-            <Alert className="bg-amber-500" onClose={()=>{toggleSnackbar(false)}} severity="success" sx={{ width: '100%' }}>
-              Your message has been registered.
-            </Alert>
-          </Snackbar>
+          {error && (
+            <Snackbar open={open} autoHideDuration={6000} onClose={() => toggleSnackbar(false)}>
+              <Alert className="bg-red-500" onClose={() => toggleSnackbar(false)} severity="error" sx={{ width: '100%' }}>
+                {error}
+              </Alert>
+            </Snackbar>
+          )}
         </form>
       </Card>
-        </div>
-    )
+    </div>
+  )
 }
