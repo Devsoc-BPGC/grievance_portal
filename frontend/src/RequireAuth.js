dbc86581822639174d92437ebb5b196f445ddfdb
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2023-11-30 16:26:03 +0530

    warning rem

--- frontend/src/RequireAuth.js
+++ frontend/src/RequireAuth.js
@@ -1,23 +1,21 @@
-import { useEffect,useState } from "react";
-import { Navigate, Outlet, useLocation,useNavigate } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import useAuth from "./useAuth";
-const axios = require('axios')
-function UnauthorisedUser(){
-    const {setAuth} = useAuth();
-    useEffect(() => {
-        localStorage.removeItem('user');
-        alert("Error: Unauthorised User. Please contact Developers' Society BITS Goa in case you are not registered.");
-        setAuth({});
-        window.location.reload();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-    return <h1>Error 401: Unauthorised User. Please contact Developers' Society BITS Goa in case you are not registered.</h1>
-}
+
+// function UnauthorisedUser(){
+//     const {setAuth} = useAuth();
+//     useEffect(() => {
+//         localStorage.removeItem('user');
+//         alert("Error: Unauthorised User. Please contact Developers' Society BITS Goa in case you are not registered.");
+//         setAuth({});
+//         window.location.reload();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//     },[])
+//     return <h1>Error 401: Unauthorised User. Please contact Developers' Society BITS Goa in case you are not registered.</h1>
+// }
 
 function RequireAuth() {
     const { auth } = useAuth();
     const location = useLocation();
-    const navigate = useNavigate();
     return (
             auth
                 ? <Outlet />
